a675f5c03ca985e508d3f1a3f6824c6ea09c9b23
Viktor Nilsson <viktor.w.nilsson@gmail.com>
2014-08-14 15:34:28 +0200

    Added smooth scrolling

--- app/scripts/gm.js
+++ app/scripts/gm.js
@@ -1,9 +1,21 @@
+var map;
+var kville = new google.maps.LatLng(57.716689, 11.949107);
+
 function initializeGM() {
     var mapOptions = {
-        center: new google.maps.LatLng(LAT, LNG), zoom: 16
+        center: kville, 
+        zoom: 16
     };
-    var map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
+    map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
     console.log(map);
+    addMarker(kville, "Herkulesparken");
 }
 //google.maps.event.addDomListener(window, 'load', initialize);
 
+function addMarker(position, title) {
+    var marker = new google.maps.Marker({
+        position: position,
+        map: map,
+        title: title
+    });
+}
